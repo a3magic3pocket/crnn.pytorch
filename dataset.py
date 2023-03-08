0c4454e0b15a515425dcbfe49f60e6fffa71c4c4
a3magic3pocket <a3magic3pocket@gmail.com>
2023-03-08 16:18:30 +0900

    Add print for debugging.

--- dataset.py
+++ dataset.py
@@ -138,6 +138,6 @@ class alignCollate(object):
             print('tmp.size', tmp.size)
             tmps.append(transform(tmp))
         images = torch.cat([t.unsqueeze(0) for t in tmps], 0)
-        print('images[0]', images[0])
+        print('images[0]', images[0].shape)
 
         return images, labels
