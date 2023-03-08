639deb0158893384c1e8aaedb0f9e41bf577bdc9
a3magic3pocket <a3magic3pocket@gmail.com>
2023-03-08 16:59:39 +0900

    recovery.

--- my_train.py
+++ my_train.py
@@ -174,8 +174,7 @@ def trainBatch(net, criterion, optimizer):
     batch_size = cpu_images.size(0)
     print('TMP::prev cpu_images', cpu_images.shape)
     print('TMP::prev image', image.shape)
-    image = torch.Tensor.copy(cpu_images)
-    # utils.loadData(image, cpu_images)
+    utils.loadData(image, cpu_images)
     
     t, l = converter.encode(cpu_texts)
     utils.loadData(text, t)
