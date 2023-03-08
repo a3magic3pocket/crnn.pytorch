19d5f3b5e3f1982b1495e9a752a2cb9f87b59cdd
a3magic3pocket <a3magic3pocket@gmail.com>
2023-03-08 17:02:32 +0900

    test

--- utils.py
+++ utils.py
@@ -133,7 +133,8 @@ def oneHot(v, v_length, nc):
 def loadData(v, data):
     print('TMP::data.size()', data.size())
     print('TMP::prev v.data.size()', v.data.size())
-    v.data.resize_(data.size()).copy_(data)
+    # v.data.resize_(data.size()).copy_(data)
+    v.data.resize_(data.size())
     print('TMP::v.data.size()', v.data.size())
 
 
