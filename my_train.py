f192d08031b9b2c74a080e0efaa702bfdeb15782
a3magic3pocket <a3magic3pocket@gmail.com>
2023-03-08 15:20:22 +0900

    Correct typos.

--- my_train.py
+++ my_train.py
@@ -169,7 +169,7 @@ def val(net, dataset, criterion, max_iter=100):
 
 
 def trainBatch(net, criterion, optimizer):
-    data = train_iter.next()
+    data = next(train_iter)
     cpu_images, cpu_texts = data
     batch_size = cpu_images.size(0)
     utils.loadData(image, cpu_images)
