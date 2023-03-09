8d71a1a622c13f9b6660679dab7e0d289ad550a3
a3magic3pocket <a3magic3pocket@gmail.com>
2023-03-09 09:45:14 +0900

    Remove debugging print

--- my_train.py
+++ my_train.py
@@ -161,21 +161,10 @@ def val(net, dataset, criterion, max_iter=100):
         loss_avg.add(cost)
 
         _, preds = preds.max(2)
-        print('preds', preds)
-        print('prev preds.shape', preds.shape)
         # preds = preds.squeeze(2)
         preds = preds.transpose(1, 0).contiguous().view(-1)
-        print('transpose preds.shape', preds.shape)
         sim_preds = converter.decode(preds.data, preds_size.data, raw=False)
-        print('sim_preds', sim_preds)
-        print('len(sim_preds', len(sim_preds))
-        print('cpu_texts', cpu_texts)
-        print('len(cpu_texts)', len(cpu_texts))
         for pred, target in zip(sim_preds, cpu_texts):
-            print('pred', pred)
-            print('target', target)
-            import sys
-            sys.exit(1)
             if pred == target.lower():
                 n_correct += 1
 
