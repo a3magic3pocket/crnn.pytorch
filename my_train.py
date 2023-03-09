2ec3b7e7300e4a5b8545d2ff41d887996510da8b
a3magic3pocket <a3magic3pocket@gmail.com>
2023-03-09 11:14:46 +0900

    Correct typos

--- my_train.py
+++ my_train.py
@@ -103,7 +103,7 @@ if opt.pretrained != '':
     
     pretrained_dict = {k: v for k, v in pretrained_dict.items() if k in model_dict}
     
-    print('pretrained_dict', pretrained_dict.key())
+    print('pretrained_dict', pretrained_dict.keys())
     
     model_dict.update(pretrained_dict)
     
