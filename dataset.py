a074366d42c378b09a80d708b100f21c2a797be9
a3magic3pocket <a3magic3pocket@gmail.com>
2023-03-10 00:00:40 +0900

    test

--- dataset.py
+++ dataset.py
@@ -50,7 +50,7 @@ class lmdbDataset(Dataset):
             buf.seek(0)
             try:
                 img = Image.open(buf).convert('L')
-                img.save("qwerasdf" + index)
+                img.save(f"qwerasdf{index}.jpg", "jpg")
             except IOError:
                 print('Corrupted image for %d' % index)
                 return self[index + 1]
